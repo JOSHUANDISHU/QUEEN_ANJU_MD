b5479dacec10ee620176d0fa9d5848765c660a1c
Exray-bandit001 <100499722+JOSHUANDISHU@users.noreply.github.com>
2024-12-13 10:13:03 +0300

    session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=CFNn3YDJ#bAnoC7yeDjGGXM7v48NFvcgZUBWOnOcy5S32xMjY5Qk",
 };
